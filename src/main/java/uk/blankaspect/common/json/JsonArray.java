f695af7036b7ba5a3ad6b2b9ff56fb4b9cd7db6c
Andy Morgan-Richards <id883436@prng.uk>
2017-12-30 08:32:30 +0000

    Changed from a class to an interface that defines only constants.

--- src/main/java/uk/blankaspect/common/json/JsonArray.java
+++ src/main/java/uk/blankaspect/common/json/JsonArray.java
@@ -2,7 +2,7 @@
 
 JsonArray.java
 
-Class: JSON array value.
+Interface: JSON array.
 
 \*====================================================================*/
 
@@ -15,164 +15,28 @@ package uk.blankaspect.common.json;
 //----------------------------------------------------------------------
 
 
-// IMPORTS
-
-
-import uk.blankaspect.common.basictree.AbstractNode;
-import uk.blankaspect.common.basictree.ListNode;
-
-//----------------------------------------------------------------------
-
-
-// CLASS: JSON ARRAY VALUE
+// INTERFACE: JSON ARRAY
 
 
 /**
- * This class implements a JSON array.  A JSON array contains zero or more elements that are {@linkplain AbstractNode
- * JSON values}.  The elements may be of different types (eg, a mixture of JSON numbers and JSON strings).
- * <p>
- * An array begins with a '[' (U+005B) and ends with a ']' (U+005D).  Adjacent elements are separated with a ','
- * (U+002C).
- * </p>
+ * This interface defines constants that relate to JSON arrays.
  */
 
-public class JsonArray
-	extends ListNode
+public interface JsonArray
 {
 
 ////////////////////////////////////////////////////////////////////////
-//  Constructors
+//  Constants
 ////////////////////////////////////////////////////////////////////////
 
-	/**
-	 * Creates a new instance of a JSON array that has no parent and initially contains no elements.
-	 */
+	/** The character that denotes the start of a JSON array. */
+	char	START_CHAR	= '[';
 
-	public JsonArray()
-	{
-	}
+	/** The character that denotes the end of a JSON array. */
+	char	END_CHAR	= ']';
 
-	//------------------------------------------------------------------
-
-	/**
-	 * Creates a new instance of a JSON array that has the specified parent and initially contains no elements.
-	 *
-	 * @param parent
-	 *          the parent of the JSON array.
-	 */
-
-	public JsonArray(AbstractNode parent)
-	{
-		// Call superclass constructor
-		super(parent);
-	}
-
-	//------------------------------------------------------------------
-
-	/**
-	 * Creates a new instance of a JSON array that has no parent and initially contains the specified elements.
-	 *
-	 * @param elements
-	 *          the initial elements of the JSON array.
-	 */
-
-	public JsonArray(Iterable<? extends AbstractNode> elements)
-	{
-		// Call superclass constructor
-		super(null, elements);
-	}
-
-	//------------------------------------------------------------------
-
-	/**
-	 * Creates a new instance of a JSON array that has the specified parent and initially contains the specified
-	 * elements.
-	 *
-	 * @param parent
-	 *          the parent of the JSON array.
-	 * @param elements
-	 *          the initial elements of the JSON array.
-	 */
-
-	public JsonArray(AbstractNode    parent,
-					 AbstractNode... elements)
-	{
-		// Call superclass constructor
-		super(parent, elements);
-	}
-
-	//------------------------------------------------------------------
-
-	/**
-	 * Creates a new instance of a JSON array that has the specified parent and initially contains the specified
-	 * elements.
-	 *
-	 * @param parent
-	 *          the parent of the JSON array.
-	 * @param elements
-	 *          the initial elements of the JSON array.
-	 */
-
-	public JsonArray(AbstractNode                     parent,
-					 Iterable<? extends AbstractNode> elements)
-	{
-		// Call superclass constructor
-		super(parent, elements);
-	}
-
-	//------------------------------------------------------------------
-
-////////////////////////////////////////////////////////////////////////
-//  Instance methods : overriding methods
-////////////////////////////////////////////////////////////////////////
-
-	/**
-	 * Returns {@code true} if the specified object is an instance of {@code JsonArray} <i>and</i> the two arrays
-	 * contain the same number of elements <i>and</i> the elements at the same index in the two arrays are equal to each
-	 * other.
-	 *
-	 * @param  obj
-	 *           the object with which this JSON array will be compared.
-	 * @return {@code true} if <i>obj</i> is an instance of {@code JsonArray} <i>and</i> the two arrays contain the
-	 *         same number of elements <i>and</i> the elements at the same index in the two arrays are equal to each
-	 *         other; {@code false} otherwise.
-	 */
-
-	@Override
-	public boolean equals(Object obj)
-	{
-		return (obj instanceof JsonArray) && super.equals(obj);
-	}
-
-	//------------------------------------------------------------------
-
-	/**
-	 * Creates and returns a deep copy of this JSON array that has no parent.
-	 *
-	 * @return a deep copy of this JSON array that has no parent.
-	 */
-
-	@Override
-	public JsonArray clone()
-	{
-		return (JsonArray)super.clone();
-	}
-
-	//------------------------------------------------------------------
-
-	/**
-	 * Returns a string representation of this JSON array.
-	 *
-	 * @return a string representation of this JSON array.
-	 */
-
-	@Override
-	public String toString()
-	{
-		return new JsonGenerator().generate(this);
-	}
-
-	//------------------------------------------------------------------
+	/** The character that separates adjacent elements of a JSON array. */
+	char	ELEMENT_SEPARATOR_CHAR	= ',';
 
 }
 
--- src/main/java/uk/blankaspect/common/json/JsonObject.java
+++ src/main/java/uk/blankaspect/common/json/JsonObject.java
@@ -2,7 +2,7 @@
 
 JsonObject.java
 
-Class: JSON object value.
+Interface: JSON object.
 
 \*====================================================================*/
 
@@ -15,233 +15,31 @@ package uk.blankaspect.common.json;
 //----------------------------------------------------------------------
 
 
-// IMPORTS
-
-
-import java.util.Map;
-
-import uk.blankaspect.common.basictree.AbstractNode;
-import uk.blankaspect.common.basictree.MapNode;
-
-//----------------------------------------------------------------------
-
-
-// CLASS: JSON OBJECT VALUE
+// INTERFACE: JSON OBJECT
 
 
 /**
- * This class implements a JSON object.  A JSON object contains zero or more properties.  A property is a
- * name&ndash;value pair whose name is a string and whose value is a {@linkplain AbstractNode JSON value}.
- * <p>
- * An object begins with a '{' (U+007B) and ends with a '}' (U+007D).  The name and value of a property are separated
- * with a ':' (U+003A).  Adjacent properties are separated with a ',' (U+002C).
- * </p>
+ * This interface defines constants that relate to JSON objects.
  */
 
-public class JsonObject
-	extends MapNode
+public interface JsonObject
 {
 
 ////////////////////////////////////////////////////////////////////////
 //  Constants
 ////////////////////////////////////////////////////////////////////////
 
+	/** The character that denotes the start of a JSON object. */
+	char	START_CHAR	= '{';
+
+	/** The character that denotes the end of a JSON object. */
+	char	END_CHAR	= '}';
+
 	/** The character that separates the name and value of a property of a JSON object. */
-	public static final	char	NAME_VALUE_SEPARATOR_CHAR	= KEY_VALUE_SEPARATOR_CHAR;
+	char	NAME_VALUE_SEPARATOR_CHAR	= ':';
 
 	/** The character that separates adjacent properties of a JSON object. */
-	public static final	char	PROPERTY_SEPARATOR_CHAR		= PAIR_SEPARATOR_CHAR;
-
-////////////////////////////////////////////////////////////////////////
-//  Constructors
-////////////////////////////////////////////////////////////////////////
-
-	/**
-	 * Creates a new instance of a JSON object that has no parent and initially contains no properties.
-	 */
-
-	public JsonObject()
-	{
-	}
-
-	//------------------------------------------------------------------
-
-	/**
-	 * Creates a new instance of a JSON object that has the specified parent and initially contains no properties.
-	 *
-	 * @param parent
-	 *          the parent of the JSON object.
-	 */
-
-	public JsonObject(AbstractNode parent)
-	{
-		// Call superclass constructor
-		super(parent);
-	}
-
-	//------------------------------------------------------------------
-
-	/**
-	 * Creates a new instance of a JSON object that has no parent and initially contains the specified properties.
-	 *
-	 * @param properties
-	 *          the initial properties of the JSON object.
-	 */
-
-	public JsonObject(Map<String, AbstractNode> properties)
-	{
-		// Call superclass constructor
-		super(properties);
-	}
-
-	//------------------------------------------------------------------
-
-	/**
-	 * Creates a new instance of a JSON object that has the specified parent and initially contains the specified
-	 * properties.
-	 *
-	 * @param parent
-	 *          the parent of the JSON object.
-	 * @param properties
-	 *          the initial properties of the JSON object.
-	 */
-
-	public JsonObject(AbstractNode              parent,
-					  Map<String, AbstractNode> properties)
-	{
-		// Call superclass constructor
-		super(parent, properties);
-	}
-
-	//------------------------------------------------------------------
-
-////////////////////////////////////////////////////////////////////////
-//  Instance methods : overriding methods
-////////////////////////////////////////////////////////////////////////
-
-	/**
-	 * Returns {@code true} if the specified object is an instance of {@code JsonObject} <i>and</i> the two objects
-	 * contain the same number of properties <i>and</i> the sets of properties of the two objects are equal to each
-	 * other.
-	 *
-	 * @param  obj
-	 *           the object with which this JSON object will be compared.
-	 * @return {@code true} if <i>obj</i> is an instance of {@code JsonObject} <i>and</i> the two objects contain the
-	 *         same number of properties <i>and</i> the sets of properties of the two objects are equal to each other;
-	 *         {@code false} otherwise.
-	 */
-
-	@Override
-	public boolean equals(Object obj)
-	{
-		return (obj instanceof JsonObject) && super.equals(obj);
-	}
-
-	//------------------------------------------------------------------
-
-	/**
-	 * Creates and returns a deep copy of this JSON object that has no parent.
-	 *
-	 * @return a deep copy of this JSON object that has no parent.
-	 */
-
-	@Override
-	public JsonObject clone()
-	{
-		return (JsonObject)super.clone();
-	}
-
-	//------------------------------------------------------------------
-
-	/**
-	 * Returns a string representation of this JSON object.
-	 *
-	 * @return a string representation of this JSON object.
-	 */
-
-	@Override
-	public String toString()
-	{
-		return new JsonGenerator().generate(this);
-	}
-
-	//------------------------------------------------------------------
-
-////////////////////////////////////////////////////////////////////////
-//  Instance methods
-////////////////////////////////////////////////////////////////////////
-
-	/**
-	 * Returns {@code true} if this JSON object contains a property whose name is the specified name and whose value is
-	 * a {@linkplain JsonArray JSON array}.
-	 *
-	 * @param  name
-	 *           the name of the property of interest.
-	 * @return {@code true} if this JSON object contains a property whose name is <i>name</i> and whose value is a JSON
-	 *         array; {@code false} otherwise.
-	 */
-
-	public boolean hasArray(String name)
-	{
-		return getValue(name) instanceof JsonArray;
-	}
-
-	//------------------------------------------------------------------
-
-	/**
-	 * Returns {@code true} if this JSON object contains a property whose name is the specified name and whose value is
-	 * a {@linkplain JsonObject JSON object}.
-	 *
-	 * @param  name
-	 *           the name of the property of interest.
-	 * @return {@code true} if this JSON object contains a property whose name is <i>name</i> and whose value is a JSON
-	 *         object; {@code false} otherwise.
-	 */
-
-	public boolean hasObject(String name)
-	{
-		return getValue(name) instanceof JsonObject;
-	}
-
-	//------------------------------------------------------------------
-
-	/**
-	 * Returns the {@linkplain JsonArray JSON array} that is associated with the specified name in this JSON object.
-	 *
-	 * @param  name
-	 *           the name of the property whose value is required.
-	 * @return the JSON-array property that is associated with <i>name</i> in this JSON object, or {@code null} if this
-	 *         JSON object does not contain a property with such a name.
-	 * @throws ClassCastException
-	 *           if this JSON object contains a property with the specified name and its value is not an instance of
-	 *           {@link JsonArray}.
-	 */
-
-	public JsonArray getArray(String name)
-	{
-		return (JsonArray)getValue(name);
-	}
-
-	//------------------------------------------------------------------
-
-	/**
-	 * Returns the {@linkplain JsonObject JSON object} that is associated with the specified name in this JSON object.
-	 *
-	 * @param  name
-	 *           the name of the property whose value is required.
-	 * @return the JSON-object property that is associated with <i>name</i> in this JSON object, or {@code null} if this
-	 *         JSON object does not contain a property with such a name.
-	 * @throws ClassCastException
-	 *           if this JSON object contains a property with the specified name and its value is not an instance of
-	 *           {@link JsonObject}.
-	 */
-
-	public JsonObject getObject(String name)
-	{
-		return (JsonObject)getValue(name);
-	}
-
-	//------------------------------------------------------------------
+	char	PROPERTY_SEPARATOR_CHAR		= ',';
 
 }
 
