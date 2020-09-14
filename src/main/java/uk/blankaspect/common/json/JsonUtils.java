9f97fc6eedaf9195b49b0cf9eb2a10becccc35ff
Andy Morgan-Richards <id883436@prng.uk>
2020-09-14 22:14:42 +0100

    Added the 'containsText' methods.
    The 'readFile' method now opens a character stream on file instead of calling 'readText'.

--- src/main/java/uk/blankaspect/common/json/JsonUtils.java
+++ src/main/java/uk/blankaspect/common/json/JsonUtils.java
@@ -18,7 +18,9 @@ package uk.blankaspect.common.json;
 // IMPORTS
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.Reader;
 
 import java.nio.charset.StandardCharsets;
 
@@ -46,7 +48,7 @@ public class JsonUtils
 ////////////////////////////////////////////////////////////////////////
 
 	/** The string that is prefixed to the name of a temporary file. */
-	private static final	String	TEMP_FILE_PREFIX	= "_$_";
+	private static final	String	TEMPORARY_FILENAME_PREFIX	= "_$_";
 
 ////////////////////////////////////////////////////////////////////////
 //  Constructors
@@ -114,10 +116,185 @@ public class JsonUtils
 	//------------------------------------------------------------------
 
 	/**
-	 * Reads the specified file, parses its content as JSON text and returns the resulting JSON value.
+	 * Returns {@code true} if the specified sequence of characters is found in the specified file, which is expected to
+	 * contain text with the UTF-8 character encoding.
 	 *
 	 * @param  file
-	 *           the file that will be read and parsed as JSON text.
+	 *           the file that will be searched.
+	 * @param  target
+	 *           the sequence of characters that will be searched for in <i>file</i>.
+	 * @return {@code true} if <i>file</i> contains <i>target</i>.
+	 * @throws IOException
+	 *           if an error occurred when reading the file.
+	 */
+
+	public static boolean containsText(Path         file,
+									   CharSequence target)
+		throws IOException
+	{
+		// Validate arguments
+		if (file == null)
+			throw new IllegalArgumentException("Null file");
+		if (target == null)
+			throw new IllegalArgumentException("Null target");
+
+		// Open character stream on file and search for target in stream
+		BufferedReader reader = null;
+		try
+		{
+			// Open character stream on file
+			reader = Files.newBufferedReader(file);
+
+			// Search for target in character stream and return the result
+			return containsText(reader, target);
+		}
+		finally
+		{
+			// Close stream
+			if (reader != null)
+			{
+				try
+				{
+					reader.close();
+				}
+				catch (IOException e)
+				{
+					// ignore
+				}
+			}
+		}
+	}
+
+	//------------------------------------------------------------------
+
+	/**
+	 * Returns {@code true} if the specified sequence of characters is found in the specified character stream.
+	 *
+	 * @param  inputStream
+	 *           the character stream that will be searched.
+	 * @param  target
+	 *           the sequence of characters that will be searched for in <i>inputStream</i>.
+	 * @return {@code true} if <i>target</i> is found in <i>inputStream</i>.
+	 * @throws IOException
+	 *           if an error occurred when reading from the input stream.
+	 */
+
+	public static boolean containsText(Reader       inputStream,
+									   CharSequence target)
+		throws IOException
+	{
+		// Validate arguments
+		if (inputStream == null)
+			throw new IllegalArgumentException("Null input stream");
+		if (target == null)
+			throw new IllegalArgumentException("Null target");
+
+		// Get length of target
+		int targetLength = target.length();
+
+		// If target is empty, it is deemed to be found in any input
+		if (targetLength == 0)
+			return true;
+
+		// Create array of target characters
+		char[] targetChars = new char[targetLength];
+		for (int i = 0; i < targetLength; i++)
+			targetChars[i] = target.charAt(i);
+
+		// Initialise circular buffer for characters from input stream
+		char buffer[] = new char[targetLength];
+		int index = 0;
+		boolean full = false;
+
+		// Read from input stream until end of input stream is reached or target is found
+		while (true)
+		{
+			// Read next character from input stream
+			int ch = inputStream.read();
+
+			// If end of stream, stop
+			if (ch < 0)
+				break;
+
+			// Add character to tail of buffer
+			buffer[index++] = (char)ch;
+
+			// If index is at end of buffer, wrap it around
+			if (index >= targetLength)
+			{
+				index = 0;
+				full = true;
+			}
+
+			// If buffer is full, compare it with target
+			if (full)
+			{
+				// Initialise index to target characters
+				int i = 0;
+
+				// Case: head of buffer is at start of array
+				if (index == 0)
+				{
+					// Compare buffer with target
+					while (i < targetLength)
+					{
+						if (targetChars[i] != buffer[i++])
+						{
+							i = -1;
+							break;
+						}
+					}
+				}
+
+				// Case: head of buffer is not at start of array
+				else
+				{
+					// Compare front part of buffer, from head of buffer to end of array, with front part of target
+					int j = index;
+					int length = targetLength - index;
+					while (i < length)
+					{
+						if (targetChars[i++] != buffer[j++])
+						{
+							i = -1;
+							break;
+						}
+					}
+
+					// If front parts matched, compare back part of buffer, from start of array to tail of buffer, with
+					// back part of target
+					if (i >= 0)
+					{
+						j = 0;
+						while (i < targetLength)
+						{
+							if (targetChars[i++] != buffer[j++])
+							{
+								i = -1;
+								break;
+							}
+						}
+					}
+				}
+
+				// If buffer matched target, indicate target was found
+				if (i == targetLength)
+					return true;
+			}
+		}
+
+		// Indicate target was not found
+		return false;
+	}
+
+	//------------------------------------------------------------------
+
+	/**
+	 * Opens a character stream on the specified file, which is expected to contain text with the UTF-8 character
+	 * encoding, parses the character stream as JSON text and returns the resulting JSON value.
+	 *
+	 * @param  file
+	 *           the file whose content will be parsed as JSON text.
 	 * @return the JSON value that results from parsing the content of <i>file</i>, if the file contains valid JSON
 	 *         text.
 	 * @throws IOException
@@ -129,14 +306,37 @@ public class JsonUtils
 	public static AbstractNode readFile(Path file)
 		throws IOException, JsonParser.ParseException
 	{
-		// Read the file; parse its content as JSON text and return the resulting JSON value
-		return new JsonParser().parse(readText(file));
+		BufferedReader reader = null;
+		try
+		{
+			// Open character stream on file
+			reader = Files.newBufferedReader(file);
+
+			// Parse character stream as JSON text and return the resulting JSON value
+			return new JsonParser().parse(reader);
+		}
+		finally
+		{
+			// Close stream
+			if (reader != null)
+			{
+				try
+				{
+					reader.close();
+				}
+				catch (IOException e)
+				{
+					// ignore
+				}
+			}
+		}
 	}
 
 	//------------------------------------------------------------------
 
 	/**
-	 * Reads the specified file and returns the resulting text, which is assumed to have the UTF-8 character encoding.
+	 * Reads the specified file, which is expected to contain text with the UTF-8 character encoding, and returns the
+	 * resulting text.
 	 *
 	 * @param  file
 	 *           the file that will be read.
@@ -228,7 +428,7 @@ public class JsonUtils
 				Files.createDirectories(parent);
 
 			// Create a temporary file in the parent directory of the output file
-			tempFile = Files.createTempFile(parent, TEMP_FILE_PREFIX, null);
+			tempFile = Files.createTempFile(parent, TEMPORARY_FILENAME_PREFIX, null);
 
 			// Write the text to the temporary file
 			Files.write(tempFile, text.getBytes(StandardCharsets.UTF_8));
